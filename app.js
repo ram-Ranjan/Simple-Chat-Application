cbf80edbcfa25d0ecd1aee2d936195a0a8f7d9d8
ram-Ranjan <ranjan.irl3107@gmail.com>
2024-05-27 14:06:10 +0530

    username issues resolved

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send(`
-    <h3>Welcome to Chatter</h3>
-    <a href="/login">Login</a>
+    <h2>Welcome to Chatter    </h2>
+
+   <a href="/login">Login</a>
   `);
 });
 
@@ -19,11 +20,9 @@ app.get('/login', (req, res) => {
   res.send(`
     <h3>Login</h3>
     <form action="/login" method="POST">
-      <input type="text" name="username" id="username" placeholder="Enter your username">
+      <input type="text" name="username"  placeholder="Enter your username">
       <button type="submit">Login</button>
-    </form>
-
-   
+    </form>   
   `);
 });
 
@@ -31,21 +30,18 @@ app.get('/login', (req, res) => {
 
 app.post('/login',(req,res) => {
 
-  let userName=req.body.username;
+  let username=req.body.username;
   //console.log(userName)
-  if(userName){
+  if(username){
+
+    res.set('content-type','text/html')
 
     res.send(`
-    <h2>Welcome to Chatter</h2>
-    <h3>Logged User: ${userName}</h3> 
-    <a href="/chat">Go to Chat</a>
-  
-      <a href="/login">Another user Login</a>
+
     <script>
-    window.localStorage.setItem("userName","${userName}");//got current user
-    // window.location.href = '/chat';
-  
-  
+    window.localStorage.setItem('username','${username}');//got current user
+     window.location.href = '/chat';
+
   </script>
     `)
   }
@@ -56,21 +52,21 @@ app.post('/login',(req,res) => {
 })
 
 const path = require('path');
-const filePath = path.join(__dirname,'message.txt');
+const filePath = path.join(__dirname,'messages.txt');
 
 app.get('/chat',(req,res) => {
-  //standerd Header of username
-  const username = req.header['x-username'] || '';
+  //standard Header of username
+  const username = req.headers['x-username'] || '';
 
   //Atempting to readFile
-  fs.readFile(filePath,'utf-8',(err,data) => {
-    if(err && err.code ==='ENDENT'){
+  fs.readFile(filePath,'utf8',(err,data) => {
+    if(err && err.code ==='ENOENT'){
 
       //If failed Creating new file
       fs.writeFile(filePath,'',(writeErr) => {
         if(writeErr){
           console.error('Error creating file:',writeErr)
-          return res.status(500).send('Internal Server ')
+          return res.status(500).send('Not able to write ')
         }
         sendChatPage(res,username,'');
     
@@ -79,70 +75,76 @@ app.get('/chat',(req,res) => {
 }
   else if(err){
     console.error('Error reading file: ',err);
-    return res.status(500).send('Internal server error')
-  }else 
+    return res.status(500).send('Unable to read')
+  }else {
+    sendChatPage(res,username,data);
+
+  }
   //method for redenring the read data
-      sendChatPage(res,username,data);
-    })
-  })
+    });
+  });
   
 
+  app.post('/chat',(req,res) => {
 
-  function sendChatPage(res,username,messageData){
+    const message  = req.body.message;
+    const username = req.headers['x-username'] || req.body.username || '';
+  
+    if(message){
+      const messageWithUsername = `${username} : ${message}\n`
+      fs.appendFile(filePath,messageWithUsername,(err) => {
+        if(err){
+        console.error('Error Creating file:',err);
+       return res.status(500).send('Message not exist')
+      }else{
+        res.redirect('/chat');
 
-    const messageHtml = messageData ? `<h4>${messageData.split('\n').map(line => `<p>${line}</p>`).join('')}<\h4>`:'';
+      }
+    });
+  }
+     else
+       res.redirect('/chat');
+  
+  });
+
+
+  function sendChatPage(res,username,messagesData){
+
+    const messagesHtml = messagesData 
+    ? `<h4>${messagesData.split('\n').map(line => `<p>${line}</p>`).join('')}</h4>` : '';
     
+    const loggedInMessage = username ? `<p>Logged in as: ${username}</p>` : '';
+
     res.send(`
-    ${messageHtml}
+    ${messagesHtml}
+
     <h3>Chatter</h3>
-    <p>Logged in as : ${username}</p>
   
-    <form id="formData" action="/chat" method="POST">
-    <input type="text" name=msg placeholder="Type your message" id="msg">
+    <form  action="/chat" method="POST">
+    <input type="text" name="message" placeholder="Type your message" id="message">
     <button type="submit" >Send</button>
     </form>
   
     <script>
-    const storedUserName=window.localStorage.getItem('username');
-    if(storedUserName){
-      document.querySelectorAll('form).forEach(form => {
-        cosnt userNameInput = documet.createElement('input');
-        userNameInput.type='hidden';
-        userNameInput.name='username';
-
-        userNameInput.value = storedUserName;
-        form.appendChild(userNameInlut);
+    const storedUsername = window.localStorage.getItem('username');
+    if(storedUsername){
+      document.querySelectorAll('form').forEach(form => {
+        const usernameInput = document.createElement('input');
+        usernameInput.type='hidden';
+        usernameInput.name='username';
+        usernameInput.value = storedUsername;
+        form.appendChild(usernameInput);
       });
-    }else
-    [
-      window.location.href = '/login'
-    }
+    }else{
+      window.location.href = '/chat'
 
+    }
     </script>
 
       `);
     }
 
-    app.post('/chat',(req,res) => {
 
-      const message  = req.body.msg;
-      const userName = req.headers['x-username'] || req.body.username || '';
-    
-      if(message){
-        const data = `${userName} : ${message}\n`
-        fs.appendFile(filePath,data,(err) => {
-          if(err){
-          console.error(err);
-          res.status(500).send('Message not exist')
-    
-        }
-        res.redirect('/chat');
-      });
-    }
-       else
-         res.redirect('/chat');
-    
-    });
 
 
 
